dac62e794d45dc4df8bd334ead76206f925184be
lewis <19032@wc.school.nz>
2022-08-11 12:30:26 +1200

    Adding title screen and difficulty buttons

--- WaveGameAwesome2/Assets/scripts/Obstacle.cs
+++ WaveGameAwesome2/Assets/scripts/Obstacle.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.SceneManagement;
+//using UnityEngine.SceneManagement;
 
 public class Obstacle : MonoBehaviour
 {
     public float speed = 20.0f; //speed that the obstacles move towards the player
     private float leftBoundry = -30; //distance at which obstacles get deleted.
-    public TextMeshProUGUI DeathText; //Text that displays when you have died
-    public GameObject RestartButton; //button to restart the game
-    public GameObject WaveDown;
-    public GameObject WaveUp;
+    //public TextMeshProUGUI DeathText; //Text that displays when you have died
+    //public GameObject RestartButton; //button to restart the game
+    //public GameObject WaveDown;
+    //public GameObject WaveUp;
     //private PlayerMovement MovementScript;
     //private PlayerTwoMovement MovementScript2;
 
@@ -21,26 +21,30 @@ public class Obstacle : MonoBehaviour
         //MovementScript2 = WaveUp.GetComponent<PlayerTwoMovement>();
     }
 
-    void OnTriggerEnter(Collider other)
+    void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
 
         if (transform.position.z < leftBoundry)
         {
             Destroy(gameObject);
         }
+    }
 
-        if (other.CompareTag("EvilTriangle"))
-        {
-            DeathText.gameObject.SetActive(true);
-            RestartButton.gameObject.SetActive(true);
-            Destroy(gameObject);
+    void OnTriggerEnter(Collider other)
+    {
+
+        //if (other.CompareTag("EvilTriangle"))
+        //{
+         //   DeathText.gameObject.SetActive(true);
+         //   RestartButton.gameObject.SetActive(true);
+         //   Destroy(gameObject);
             //MovementScript.DisableScript();
             //MovementScript2.DisableScript();
-        }
+       //}
     }
-    public void RestartGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+    //public void RestartGame()
+    //{
+    //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    //}
 }
