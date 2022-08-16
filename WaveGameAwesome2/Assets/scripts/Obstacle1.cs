a66ed29e481439fac292a6d17ab76ed188bb847b
lewis <19032@wc.school.nz>
2022-08-17 11:11:55 +1200

    Added restart button, obstacle
    Starting trying to fix obstacle code created new better obstacle added restart button started working on all the buttons and menu/restart screens

--- WaveGameAwesome2/Assets/scripts/Obstacle1.cs
+++ WaveGameAwesome2/Assets/scripts/Obstacle1.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Obstacle1 : MonoBehaviour
 {
     public float speed = 20.0f; //speed that the obstacles move towards the player
     private float leftBoundry = -30; //distance at which obstacles get deleted.
+    public TextMeshProUGUI DeathText; //Text that displays when you have died
+    public GameObject RestartButton; //button to restart the game
+    //public GameObject WaveDown;
+    //public GameObject WaveUp;
+    //private PlayerMovement movementScript;
+    //private PlayerTwoMovement movementScript2;
 
     void Start()
     {
-
+        //movementScript = WaveDown.GetComponent<PlayerMovement>();
+        //movementScript2 = WaveUp.GetComponent<PlayerTwoMovement>();
     }
 
     void Update()
@@ -20,5 +29,22 @@ public class Obstacle1 : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            DeathText.gameObject.SetActive(true);
+            restartButton.gameObject.SetActive(true);
+            Destroy(gameObject);
+            //movementScript.DisableScript();
+            //movementScript2.DisableScript();
+       }
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
