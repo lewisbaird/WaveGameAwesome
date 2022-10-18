38d5d60a053be30142112ef56c14d092e5ff5ccd
lewis <19032@wc.school.nz>
2022-10-18 15:14:19 +1300

    LEVELCOMPLETE THING

---
+++ AwesomeWaveGame2D/Assets/Scripts/LevelCompletion.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCompletion : MonoBehaviour
+{
+    public GameObject levelEasy;
+    public GameObject levelNormal;
+    public GameObject levelHard;
+    public GameObject levelImpossible;
+    public GameObject background;
+    public GameObject playerOne;
+    public GameObject playerTwo;
+    public GameObject completetionWall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            levelEasy.gameObject.SetActive(true);
+            levelNormal.gameObject.SetActive(true);
+            levelHard.gameObject.SetActive(true);
+            levelImpossible.gameObject.SetActive(true);
+            background.gameObject.SetActive(true);
+            playerOne.gameObject.SetActive(false);
+            playerTwo.gameObject.SetActive(false);
+            completetionWall.gameObject.SetActive(false);
+        }
+    }
+
+}
